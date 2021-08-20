8cd11cc5e5607ddb3b5a216d1e33c9bf47698f6e
Karol de Castro <karolinycosta@yahoo.com.br>
2021-08-20 11:46:37 -0300

    Exercicios heranca - account Program

---
+++ src/main/java/br/com/cursoNelioAlves/heranca/ex01/application/AccountProgram.java
@@ -0,0 +1,17 @@
+package br.com.cursoNelioAlves.heranca.ex01.application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AccountProgram {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+
+        System.out.println("Enter client data: ");
+    }
+}
