17620335c8e66015804431328a7817eed4c71b71
knigosky <karolinycosta@yahoo.com.br>
2021-09-01 16:45:20 -0300

    lendo arquivos em texto

---
+++ src/main/java/br/com/cursoNelioAlves/arquivos/fileReaderAndBufferedReader/Program.java
@@ -0,0 +1,42 @@
+package br.com.cursoNelioAlves.arquivos.fileReaderAndBufferedReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        String path = ("//Users//knigosky//Downloads//_Lorem Ipsum.docx");
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
