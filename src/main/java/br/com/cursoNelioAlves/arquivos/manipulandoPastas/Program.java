d7640c71e4ea83913c05ff6c1b2cc0130a98a734
knigosky <karolinycosta@yahoo.com.br>
2021-09-02 17:29:04 -0300

    manipulacao de pastas - criando pastas com o new File(strPath + "//subdir").mkdir();

--- src/main/java/br/com/cursoNelioAlves/arquivos/manipulandoPastas/Program.java
+++ src/main/java/br/com/cursoNelioAlves/arquivos/manipulandoPastas/Program.java
@@ -15,17 +15,18 @@ public class Program {
 
         File[] folders = path.listFiles(File::isDirectory);
         System.out.println("FOLDERS: ");
-
         for (File folder : folders) {
             System.out.println(folder);
         }
 
         File[] files = path.listFiles(File::isFile);
-        System.out.println("FOLDERS: ");
-
+        System.out.println("FILES: ");
         for (File file : files) {
             System.out.println(file);
         }
+
+        boolean success = new File(strPath + "//subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
         sc.close();
     }
 }
